6c521ae989e9f206bc531bccdc8068383dacc242
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2018-02-25 19:24:10 +0100

    now generating for sobol as well

--- configs_euler_brownian/run_sobol.sh
+++ configs_euler_brownian/run_sobol.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-NX=256
+NX=64
 export OMP_NUM_THREADS=1
 cd "N${NX}"
 DIMENSIONS=$(($NX*$NX*2))
@@ -9,15 +9,16 @@ do
 
 
     cd "H_${HURINDEX}";
-    for generator in "monte_carlo" "halton_large" "halton409_large";
+    for generator_folder in euler_brownian_*;
     do
 
-	cd "euler_brownian_${generator}";
+	cd ${generator_folder}
 	for samples in 64 128 256 512 1024 2048 4096 8192 $((2*8192)) $((4*8192)) $((8*8192)) $((16*8192))
 	do
 
 	    cd "m${samples}";
 	    cp -r ../../../../*.so ./
+	    cp -r ../../../../sobol ./
 
 	    MULTI_SAMPLES=1;
 	    
@@ -25,6 +26,7 @@ do
 		then
 		MULTI_SAMPLES=$(($samples/128))
 	    fi
+	    echo ${generator_folder}
 	    bsub -R beta -W 24:00 -N -B -n ${MULTI_SAMPLES} mpirun -np ${MULTI_SAMPLES} $HOME/alsvinn/build/alsuqcli/alsuqcli --multi-sample ${MULTI_SAMPLES} euler_brownian/euler_brownian.xml
 
 	    cd ..;
